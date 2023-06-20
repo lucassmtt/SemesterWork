db19a105e679c2bebd30c16ce342bcce079e0f5f
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-06-20 18:10:54 -0300

    Adjusting files of config container...

---
+++ Resources/src/db/DB.java
@@ -0,0 +1,17 @@
+package db;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class DB
+{
+    public static Properties loadProperties()
+    {
+        Properties properties = new Properties();
+        try(FileInputStream fileInputStream = new FileInputStream(
+                ""
+        )) {
+
+        }
+    }
+}
